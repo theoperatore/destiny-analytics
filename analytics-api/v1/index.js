31c10ad96ab0d981c8493c96b967a02f889960db
theoperatore <theoperatore@gmail.com>
2016-10-26 22:50:43 -0400

    get basic membership stuffs when requesting schedules

--- analytics-api/v1/index.js
+++ analytics-api/v1/index.js
@@ -3,8 +3,15 @@
 const daService = require('destiny-analytics-service');
 const Router = require('express').Router;
 const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
 const cors = require('cors');
 const debug = require('debug')('da:routes');
+
+const classMap = {
+  '671679327': 'Hunter',
+  '2271682572': 'Warlock',
+  '3655393761': 'Titan'
+};
 
 class ValidationError extends Error {
   constructor(message) {
@@ -106,6 +113,24 @@ module.exports = function createV1Api(creds) {
     return svc
       .ready()
       .then(() => svc.getAllSchedules())
+      .then(schedules => {
+        return Promise.all(schedules.map(sched => {
+          return sched.membershipId
+            ? fetch(`http://www.bungie.net/Platform/Destiny/2/Account/${sched.membershipId}/Summary/`)
+                .then(response => response.json())
+                .then(response => response.Response && response.Response.data.characters.map(char => ({ 
+                  emblemPath: char.emblemPath,
+                  backgroundPath: char.backgroundPath,
+                  className: classMap[char.characterBase.classHash],
+                })))
+            : [];
+        }))
+        .then(charactersPerSchedule => {
+          return schedules.map((sched, i) => Object.assign({}, sched, {
+            meta: charactersPerSchedule[i],
+          }));
+        })
+      })
       .then(schedules => {
         res
           .status(200)
