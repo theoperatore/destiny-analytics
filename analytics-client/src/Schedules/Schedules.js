3dfb3a71643fb84ec02f97f592bbb87baf5cdaf7
theoperatore <theoperatore@gmail.com>
2016-10-26 23:27:41 -0400

    initial view of schedules

--- analytics-client/src/Schedules/Schedules.js
+++ analytics-client/src/Schedules/Schedules.js
@@ -3,14 +3,54 @@ import { fetchAllSchedules } from '../state/schedules/actions';
 
 import './schedules.css';
 
+function Schedule(props) {
+  const style = {
+    backgroundImage: `url(https://bungie.net${props.background})`,
+    backgroundRepeat: 'no-repeat',
+  }
+  return <div
+    className='schedule'
+    onClick={props.onClick}
+    style={style}
+  >
+    <img
+      src={`https://bungie.net${props.emblem}`}
+      width='75'
+      height='75'
+      alt='destiny-class-emblem'
+    />
+    <div className='schedule-meta'>
+      <p>{props.name}</p>
+      <p>{props.dclass}</p>
+    </div>
+  </div>
+}
+
 export default class Schedules extends Component {
   componentDidMount() {
     this.props.dispatch(fetchAllSchedules());
   }
 
+  handleScheduleSelect(sched) {
+    console.log(sched);
+  }
+
   renderSchedules() {
     return this.props.schedules.map((sched, i) => {
-      return <p key={i}><strong>{sched.username}</strong> - {sched.id}</p>
+      return <div className='player-schedules' key={i}>
+        {
+          sched.meta.map((meta, j) => {
+            return <Schedule
+              key={`${i}${j}`}
+              onClick={this.handleScheduleSelect.bind(this, sched)}
+              name={sched.username}
+              emblem={meta.emblemPath}
+              background={meta.backgroundPath}
+              dclass={meta.className}
+            />
+          })
+        }
+      </div>
     });
   }
 
@@ -18,7 +58,7 @@ export default class Schedules extends Component {
     return <div className='schedules'>
       {
         this.props.scheduleStatus.loading
-          ? <p>Loading...</p>
+          ? <p>Loading schedule meta data...</p>
           : this.renderSchedules()
       }
     </div>
